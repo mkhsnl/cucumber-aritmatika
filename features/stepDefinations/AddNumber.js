de8ee5521ef86b232ccdbc0e50e5cac03a60cb62
mkhsnl <makhasinularif@gmail.com>
2024-11-01 10:33:55 +0700

    01-11

--- features/stepDefinations/AddNumber.js
+++ features/stepDefinations/AddNumber.js
@@ -1,79 +1,72 @@
- const {Given, When, Then}= require('@cucumber/cucumber')
+const { Given, When, Then } = require('@cucumber/cucumber');
+const prompt = require('prompt-sync')();
 
+let num1, num2, sum;
 
+// Pengujian Penjumlahan
 Given('Enter numbers for Adding', function () {
-    num1 = 5;
-    num2 = 10;
-    return console.log('Numbers are'+' '+num1+' '+'and'+' '+num2);
-  });
-
-
+    num1 = parseFloat(prompt('input first number (+) : '));
+    num2 = parseFloat(prompt('input second number (+) : '));
+    console.log('Numbers are ' + num1 + ' and ' + num2);
+});
 
 When('Calculate Adding', function () {
-    sum = num1+num2;
-    return console.log(' ' +num1 + '+' +num2 + '=');
-  });
-
-
+    sum = num1 + num2;
+    console.log(num1 + ' + ' + num2 + ' =');
+});
 
 Then('Result of Adding', function () {
-    return console.log('Sum of numbers are'+' '+sum);
-  });
+    console.log('Sum of numbers is ' + sum);
+    console.log('=====================================');
+});
 
 
+// Pengujian Pengurangan
+Given('Enter numbers for Subtraction', function () {
+    num1 = parseFloat(prompt('input first number (-) : '));
+    num2 = parseFloat(prompt('input second number (-) : '));
+    console.log('Numbers are ' + num1 + ' and ' + num2);
+});
 
-         Given('Enter numbers for Subtraction', function () {
-           num1 = 8;
-           num2 = 3;
-           return console.log('Numbers are'+' '+num1+' '+'and'+' '+num2);
-         });
+When('Calculate Subtraction', function () {
+    sum = num1 - num2;
+    console.log(num1 + ' - ' + num2 + ' =');
+});
 
+Then('Result of Subtraction', function () {
+    console.log('Result of subtraction is ' + sum);
+    console.log('=====================================');
+});
 
-         When('Calculate Subtraction', function () {
-           sum = num1-num2;
-           return console.log(' ' +num1 + '-' +num2 + '=');
-         });
+// Pengujian Perkalian
+Given('Enter numbers for Multiplication', function () {
+    num1 = parseFloat(prompt('input first number (*) : '));
+    num2 = parseFloat(prompt('input second number (*) : '));
+    console.log('Numbers are ' + num1 + ' and ' + num2);
+});
 
-  
-         Then('Result of Subtraction', function () {
-           
-           return console.log('Sum of number are'+' '+sum);
-         });
+When('Calculate multiplication', function () {
+    sum = num1 * num2;
+    console.log(num1 + ' * ' + num2 + ' =');
+});
 
+Then('Result of Multiplication', function () {
+    console.log('Result of multiplication is ' + sum);
+    console.log('=====================================');
+});
 
-            Given('Enter numbers for Multiplication', function () {
-                num1 = 4;
-                num2 = 6;
-                return console.log('Numbers are'+' '+num1+' '+'and'+' '+num2);
-                });
- 
- 
-             When('Calculate multiplication', function () {
-                sum = num1*num2;
-                return console.log(' ' +num1 + '*' +num2 + '=');
-                });
- 
-   
-            Then('Result of Multiplication', function () {
-                
-                return console.log('Result of multiplication is'+' '+sum);
-                });
+// Pengujian Pembagian
+Given('Enter numbers for Distribution', function () {
+    num1 = parseFloat(prompt('input first number (/) : '));
+    num2 = parseFloat(prompt('input second number (/) : '));
+    console.log('Numbers are ' + num1 + ' and ' + num2);
+});
 
+When('Calculate Distribution', function () {
+    sum = num1 / num2;
+    console.log(num1 + ' / ' + num2 + ' =');
+});
 
-                    Given('Enter numbers for Distribution', function () {
-                        num1 = 9;
-                        num2 = 3;
-                        return console.log('Numbers are'+' '+num1+' '+'and'+' '+num2);
-                    });
-     
-     
-                    When('Calculate Distribution', function () {
-                        sum = num1 / num2;
-                        return console.log(' ' +num1 +'/' + num2 + '=');
-                    });
-     
-       
-                    Then('Result of Distribution', function () {
-                    
-                        return console.log('Result of distribution is'+' '+sum);
-                    });
+Then('Result of Distribution', function () {
+    console.log('Result of division is ' + sum);
+});
